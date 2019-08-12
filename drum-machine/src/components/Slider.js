4d2b0b85e386f39fe02d444e3eb25c52c9e2ca2d
Archbold <archiboldpatsanza@gmail.com>
2019-08-12 20:35:42 +0300

    buttons

--- drum-machine/src/components/Slider.js
+++ drum-machine/src/components/Slider.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { Progress } from 'reactstrap';
 import '../App.css';
+import {Button} from 'reactstrap';
+import {Row} from 'reactstrap'
 //componets start here
 function Slider(props){
-  return(<Progress className="slider" value="25" color='success'/>)
+  return(<div className="slider">
+          <Progress  value="25" color='success'/>
+          <div className="buttons container">
+            <Row>
+              <i className='fas fa-volume-up' style={{fontSize:'30px',color:'grey'}}></i>
+            </Row>
+            <Row>
+            <Button className="button increase">+</Button>
+            </Row>
+            <Row>
+              <Button className="button decrease">-</Button>
+            </Row>
+          </div>
+        </div>)
 }
 export default Slider
