305a32f4aab891a08a83129b5e1ca4ea0f51cc7f
Archbold <archiboldpatsanza@gmail.com>
2019-08-18 23:45:02 +0300

    slide on its way

--- drum-machine/src/components/Drumpad.js
+++ drum-machine/src/components/Drumpad.js
@@ -1,6 +1,7 @@
 import {Container,Row} from 'reactstrap';
 import React from 'react';
 import Drumkeys from './Drumkeys.js';
+import '../App.css'
 
 
 function Drumpad(props){
@@ -8,6 +9,13 @@ function Drumpad(props){
     <Container>
       <Row>
         <Drumkeys keys={props.keys} press={props.press} release={props.release} keyboardPress={props.keyboard}/>
+      </Row>
+      <Row style={{textAlign:'centre'}}>
+        <br/>
+        <div id="toggler">
+          <div className="mod1 toggler">
+          </div>
+        </div>
       </Row>
     </Container>
   )
